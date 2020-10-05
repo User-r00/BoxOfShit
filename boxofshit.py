7be2280b7e2fe1480830cf6704d5a477d3d94528
r00 <15057809+User-r00@users.noreply.github.com>
2020-10-05 13:15:57 +0000

    Migrate auth credentials to secrets file

---
+++ boxofshit.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Box of Shit is a bot that monitors bathroom habits to enlighten and entertain.
+
+BoS is built in Python and designed to run on a Raspberry Pi Zero W. The full
+wiring diagram can be found in the README or at r00tuser.com.
+
+Created by: r00
+Version: 0.01a
+Last modified: 10/2/2020
+'''
+
+import csv
+import datetime
+from gpiozero import Button
+from math import floor
+from time import sleep
+
+import twitter
+
+import secrets
+
+
+api = twitter.Api(consumer_key=secrets.comsumer_key,
+  				  consumer_secret=secrets.consumer_secret,
+  				  access_token_key=secrets.access_token_key,
+  				  access_token_secret=secrets.access_token_secret)
+					
+def generate_session_message(times):
+	minutes = times[2]
+	seconds = times[3]
+	return f'I pooped for {minutes} minutes and {seconds} seconds.'
+	
+def calculate_times(start, end):
+	time_delta = end - start
+	minutes = (time_delta.seconds // 60) % 60
+	seconds = time_delta.seconds - (minutes * 60)
+	
+	start_str = start.strftime('%m/%d/%Y %H:%M:%S')
+	end_str = end.strftime('%m/%d/%Y %H:%M:%S')
+	
+	return (start_str, end_str, minutes, seconds, start, end)
+
+def tweet(message):
+	status = api.PostUpdate(message)
+	print(status.text)
+	
+def save_data(times):
+	start_time = times[4].strftime('%m/%d/%Y %H:%M:%S')
+	end_time = times[5].strftime('%m/%d/%Y %H:%M:%S')
+	with open('stats.csv', 'a') as csvfile:
+		spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		spamwriter.writerow([start_time, end_time, times[2], times[3], times[4]])
+
+if __name__ == '__main__':
+	ass_switch = Button(26)
+	while True:
+		print('Waiting for ass.')
+		ass_switch.wait_for_press()
+		
+		print('Ass found. Starting timer.')
+		start = datetime.datetime.now()
+		
+		print('Waiting for ass to disappear.')
+		ass_switch.wait_for_release()
+		
+		print('Poof! Ass is gone. Ending timer.')
+		end = datetime.datetime.now()
+		
+		print('Calculating poop session length.')
+		times = calculate_times(start, end)
+		
+		print('Generating tweets with stats.')
+		message = generate_session_message(times)
+		
+		print('Sending tweet.')
+		tweet(message)
+		
+		print('Saving data to file.')
+		save_data(times)
